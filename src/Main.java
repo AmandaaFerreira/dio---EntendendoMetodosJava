a2e4db34ee5d1970ca9dae16aa2c838909a40998
Amanda <amandaferreiradasilva33@gmail.com>
2022-05-04 15:29:50 -0300

    Alteracao e modificacao classe Quadrilatero

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Main {
     public static void main(String[] args) {
         //Calculadora
@@ -19,12 +21,25 @@ public class Main {
         Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
         Emprestimo.calcular(1000, 5);
 
-        //Criação de um Quadrilatero - utilizando Sobrecarga
+        //Criação de Quadrilatero - utilizando Sobrecarga
         System.out.println("Exercicio quadrilatero");
         Quadrilatero.area(3);
         Quadrilatero.area(5D,5D);
         Quadrilatero.area(7,8,9);
         Quadrilatero.area(5F,5F);
 
+        //Criação de Quadrilatero - utilizando Retornos
+        System.out.println("Exercicio retornos");
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Area do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5D,5D);
+        System.out.println("Area do retangulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Area do trapézio: " + areaTrapezio);
+
+        float areaLosango = Quadrilatero.area(5F, 5F);
+        System.out.println("Area do losango: " + areaLosango);
     }
 }
--- src/Quadrilatero.java
+++ src/Quadrilatero.java
@@ -1,16 +1,22 @@
 public class Quadrilatero {
 
-    public static void area(double lado){
+    public static double area(double lado){
         System.out.println("Area do quadrado: " + lado * lado);
+        return lado * lado;
+    }
 
-    }
-    public static void area(double lado1, double lado2){
+    public static double area(double lado1, double lado2){
         System.out.println("Area do retangulo: " + lado1 * lado2);
-    }
-    public static void area(double baseMaior, double baseMenor, double altura ){
+        return lado1 * lado2;
+   }
+
+    public static double area(double baseMaior, double baseMenor, double altura ){
         System.out.println("Area do trapézio: " + (baseMaior + baseMenor) * altura / 2);
+        return (baseMaior + baseMenor) * altura / 2;
     }
-    public static void area (float diagonal1, float diagonal2){
-        System.out.println("Area do losango: " + (diagonal1 * diagonal2)/2);
+
+    public static float area (float diagonal1, float diagonal2){
+        System.out.println("Area do losango: " + (diagonal1 * diagonal2) / 2);
+        return (diagonal1 * diagonal2) / 2;
     }
 }
