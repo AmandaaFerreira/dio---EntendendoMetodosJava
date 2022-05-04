1922caf78365a08d843a1ee807a65defb5bdeaee
Amanda <amandaferreiradasilva33@gmail.com>
2022-05-04 14:23:03 -0300

    Acrescentando a classe Quadrilatero

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 public class Main {
-public static void main (String[] args){
-    //Calculadora
-    System.out.println("Exercicio Calculadora");
-    Calculadora.soma (3,6);
-    Calculadora.subtracao(9, 1.8);
-    Calculadora.multiplicacao(7,8);
-    Calculadora.divisao(5, 2.5);
+    public static void main(String[] args) {
+        //Calculadora
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
 
-    //Mensagem
-    System.out.println("Exercicio Mensagem");
-    Mensagem.obterMensagem(9);
-    Mensagem.obterMensagem(14);
-    Mensagem.obterMensagem(1);
+        //Mensagem
+        System.out.println("Exercicio Mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
 
-    //Empréstimo
-    System.out.println("Exercicio Empréstimo");
-    Emprestimo.calcular(1000,Emprestimo.getDuasParcelas());
-    Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
-    Emprestimo.calcular(1000,5);
+        //Empréstimo
+        System.out.println("Exercicio Empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
 
-}
+        //Criação de um Quadrilatero - utilizando Sobrecarga
+        System.out.println("Exercicio quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5D,5D);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5F,5F);
+
+    }
 }
